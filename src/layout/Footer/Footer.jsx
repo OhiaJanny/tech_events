3482f0ee074906e1378a6d0198846a39eaab5e02
OhiaJanny <janechidera12@gmail.com>
2023-03-30 10:47:16 +0100

    Feat - Added Subscribe form

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './Footer.module.css'
 import logo from '../../assets/logo.png'
 import {Link} from 'react-router-dom'
-
+import Input from '../../components/Input/Input'
+import { validateEmail } from '../../utils/validateEmail'
+import { toast } from 'react-toastify';
 
 const Footer = () => {
+
+  const [email, setEmail] = useState('')
+  const [loading, setLoading] = useState('')
+
+  const subscribe =(e)=>{
+    e.preventDefault()
+
+    if(!validateEmail(email)){
+      return toast.error('Invalid Email', {
+        position: toast.POSITION.TOP_RIGHT
+      });
+    }
+    setLoading(true)
+    fetch("https://newsletter-plne.onrender.com/subscribe", {
+        method:'post',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({email:email})
+    })
+    .then(res => res.json())
+    .then(data =>{
+      setLoading(false)
+        if(data.success){
+          toast.success(data.success, {
+            position: toast.POSITION.TOP_RIGHT
+          });
+          setEmail('')
+        }else{
+          toast.error(data.error, {
+            position: toast.POSITION.TOP_RIGHT
+          });
+        }
+    })
+    .catch(error => {
+      setLoading(false)
+      console.log(error)
+    })
+  }
+
   return (
     <footer className={styles.footer}>
         <div className='max-wrapper'>
@@ -14,6 +56,9 @@ const Footer = () => {
                 <Link to='/'><img src={logo} alt="She Codes"/></Link>
                 <p>Her Code Events</p>
             </div>
+            <form className={styles.form} onSubmit={subscribe}>
+              <Input value={email} placeholder='Email Address' onChange={(e) => setEmail(e.target.value)} /> <button >{loading ? 'Loading...' : 'Subscribe'}</button>
+            </form>
         </div>
         </div>
     </footer>
